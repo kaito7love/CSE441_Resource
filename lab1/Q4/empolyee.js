53f76f7d6e165419a55b15bf6e96eefbfb866c7f
Nam Kaito7love <104958975+kaito7love@users.noreply.github.com>
2024-10-17 14:43:31 +0700

    Nam
    Lab1 submit

---
+++ lab1/Q4/empolyee.js
@@ -0,0 +1,15 @@
+
+
+function empolyee(){
+    return(
+        <view>
+            <text>Full name</text>
+            <TextInput></TextInput>
+            <text>Age</text>
+            <TextInput></TextInput>
+            <text>Ocupation</text>
+            <TextInput></TextInput>
+            <button tille = "Update"></button>
+        </view>
+    );
+}
